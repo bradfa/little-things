14ebb4a643fe11dd0507ada9a8e3318558b6bb4e
Andrew Bradford <bradfa@gmail.com>
2020-07-16 14:25:25 -0400

    Create restic-backup.sh
    No idea if it works but just jotting it down.

---
+++ restic-backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+# Do a restic backup to the directory/device specified.
+
+if [ $# -ne 2 ]; then
+	echo
+	echo "Usage: ${0} <destination>"
+	echo
+	exit 1
+fi
+
+if [ ! -d ${1} ]; then
+	echo
+	echo "Usage: ${0} <destination>"
+	echo "<destination> must be a directory"
+	echo
+	exit 1
+fi
+
+sudo restic backup --repo=${1}/restic-repo/ --cache-dir=${1}/restic-cache/ \
+	--exclude-caches --one-file-system --password-file=/home/andrew/.restic.passwd \
+	--exclude-file=/home/andrew/.restic.excludes --verbose /etc/ /home/andrew/
